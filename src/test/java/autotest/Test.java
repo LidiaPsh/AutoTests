7ce67b7ba01726e9e466c00b84ec6426abc55cd6
LidiaPsh <70895000+LidiaPsh@users.noreply.github.com>
2024-03-20 12:13:17 +0300

    Initial commit

---
+++ src/test/java/autotest/Test.java
@@ -0,0 +1,10 @@
+package autotest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void test(){
+        assertTrue(true);
+    }
+}
