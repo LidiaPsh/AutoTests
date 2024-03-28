fd5c3a8fe41a8ec05c25ea7953c6b6c102f3fdfd
LidiaPsh <70895000+LidiaPsh@users.noreply.github.com>
2024-03-28 17:36:45 +0300

    Дз 2

--- src/test/java/autotest/TestHm1.java
+++ src/test/java/autotest/TestHm1.java
@@ -1,10 +1,10 @@
 package autotest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class Test {
+public class TestHm1 {
     @org.junit.jupiter.api.Test
     public void test(){
         assertTrue(true);
     }
+
 }
